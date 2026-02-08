7e9d7ccb3594965c9f9f257f8072c119d0abdfbd
Miyase Satsuki <satsuki@spri.in>
2026-02-08 13:50:16 +0530

    use mongo tools

--- migrate_old.py
+++ migrate_old.py
@@ -1,47 +1,30 @@
-import asyncio
-import traceback
-from os import getenv
+import lmdb
+import gc
+from sys import argv
+from pathlib import Path
+from typing import Dict, Any
 from datetime import datetime, timezone
-from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase, AsyncIOMotorCollection
+from bson import decode_file_iter, encode, decode
 from pyrogram.file_id import FileId
-from tqdm import tqdm
 
-progress_lock = asyncio.Lock()
+if len(argv)<=3 or not argv[3].isdigit():
+    print("Usage: python bson_util.py <old db name> <new db name> <bot_id>")
+    exit(1)
 
-class MongoTGFS():
-    client: AsyncIOMotorClient
-    db: AsyncIOMotorDatabase
-    files: AsyncIOMotorCollection
-    groups: AsyncIOMotorCollection
-    users: AsyncIOMotorCollection
-    config: AsyncIOMotorCollection
+DUMP_PATH = Path("mongodump", argv[2])
+DUMP_PATH.mkdir(parents=True, exist_ok=True)
 
-    async def connect(self, uri: str, dbname) -> None:
-        self.client = AsyncIOMotorClient(uri)
-        self.db = self.client[dbname]
+INPUT_BSON = Path("oldbson", argv[1])
+FILES_BSON = Path(DUMP_PATH, "files.bson")
+MAPLINKS_BSON = Path(DUMP_PATH, "maplinks.bson")
+USERS_BSON = Path(DUMP_PATH, "users.bson")
 
-        self.files = self.db.files
-        self.groups = self.db.groups
-        self.users = self.db.users
-        self.config = self.db.app_config
-        self.maplinks = self.db.maplinks
+LMDB_PATH = "./files.lmdb"
+LMDB_MAP_SIZE = 1024 * 1024 * 1024 * 200
 
-    async def close(self) -> None:
-        self.client.close()
+WRITE_BATCH = 10_000
 
-    async def init_db(self) -> None:
-        await self._create_indexes()
-
-    async def _create_indexes(self) -> None:
-        await self.files.create_index("users.user_id")
-        await self.files.create_index("is_deleted")
-        await self.files.create_index("users.added_at")
-
-        await self.groups.create_index("user_id")
-        await self.groups.create_index([("user_id", 1), ("created_at", -1)])
-
-        await self.users.create_index("ban_date")
-        await self.users.create_index("warns")
+BOT_ID = int(argv[3])
 
 
 def normalize_time(t):
@@ -51,33 +34,19 @@ def normalize_time(t):
         return datetime.fromisoformat(t)
     return datetime.fromtimestamp(float(t), tz=timezone.utc)
 
+def deep_set(target: Dict[str, Any], update: Dict[str, Any]) -> None:
+    for k, v in update.items():
+        if isinstance(v, dict) and isinstance(target.get(k), dict):
+            deep_set(target[k], v)
+        else:
+            target[k] = v
 
-BOT_ID = None
 
-async def migrate_old_file(tgfs: MongoTGFS, old: dict) -> None:
+def build_files_update(old: dict) -> tuple[int, dict]:
     decoded = FileId.decode(old["file_id"])
     media_id = decoded.media_id
-
     user_id = int(old["user_id"])
 
-    location_update = {
-        f"location.{BOT_ID}": {
-            "access_hash": decoded.access_hash,
-            "file_reference": decoded.file_reference
-        }
-    }
-
-    file_ids = old.get("file_ids")
-    if isinstance(file_ids, dict):
-        for bot_id, fid in file_ids.items():
-            try:
-                d = FileId.decode(fid)
-                location_update[f"location.{int(bot_id)}"] = {
-                    "access_hash": d.access_hash,
-                    "file_reference": d.file_reference,
-                }
-            except Exception:
-                continue
     update = {
         "dc_id": decoded.dc_id,
         "file_name": old["file_name"],
@@ -85,196 +54,128 @@ async def migrate_old_file(tgfs: MongoTGFS, old: dict) -> None:
         "mime_type": old["mime_type"],
         "size": int(old["file_size"]),
         "thumb_size": decoded.thumbnail_size,
-        f"users.{user_id}": {
-            "chat_id": user_id,
-            "message_id": old.get("msg_id") or 0,
-            "added_at": normalize_time(old["time"]),
-        },
-        **location_update,
-    }
-
-    await tgfs.files.update_one(
-        {"_id": media_id},
-        {"$set": update},
-        upsert=True,
-    )
-    await tgfs.maplinks.update_one(
-        {"_id": old["_id"]},
-        {"$set": {
-            "media_id": media_id,
-            "user_id": user_id
-        }},
-        upsert=True,
-    )
-
-
-async def migrate_old_users(tgfs: MongoTGFS, old: dict) -> None:
-    await tgfs.users.update_one(
-        {"_id": old["id"]},
-        {"$set": {
-            "_id": old["id"],
-            "ban_date": None,
-            "curt_op": 0,
-            "join_date": normalize_time(old["join_date"]),
-            "op_id": 0,
-            "preferred_lang": "en",
-            "warns": 0
-        }
-        },
-        upsert=True,
-    )
-
-
-async def migrate_blacklist(tgfs: MongoTGFS, old: dict) -> None:
-    await tgfs.users.update_one(
-        {"_id": old["id"]},
-        {
-            "$set": {
-                "ban_date": normalize_time(old["ban_date"])
+        "users": {
+            str(user_id): {
+                "chat_id": user_id,
+                "message_id": old.get("msg_id") or 0,
+                "added_at": normalize_time(old["time"]),
             }
         },
-        upsert=False
+        "location": {
+            str(BOT_ID): {
+                "access_hash": decoded.access_hash,
+                "file_reference": decoded.file_reference,
+            }
+        },
+    }
+
+    file_ids = old.get("file_ids")
+    if isinstance(file_ids, dict):
+        for bot_id, fid in file_ids.items():
+            try:
+                d = FileId.decode(fid)
+                update["location"][str(bot_id)] = {
+                    "access_hash": d.access_hash,
+                    "file_reference": d.file_reference,
+                }
+            except Exception:
+                continue
+
+    return media_id, update
+
+def migrate_files_lmdb():
+    env = lmdb.open(
+        LMDB_PATH,
+        map_size=LMDB_MAP_SIZE,
+        subdir=True,
+        max_dbs=1,
+        lock=True,
+        sync=False,
+        metasync=False,
+        readahead=True,
     )
 
-async def worker(name, queue, migrate_fn, tgfs, pbar, batch=20):
-    local_count = 0
-    while True:
-        item = await queue.get()
-        if item is None:
-            if local_count:
-                pbar.update(local_count)
-            queue.task_done()
-            break
-        try:
-            await migrate_fn(tgfs, item)
-            local_count += 1
-        except Exception as e:
-            print(f"[{name}] Error:", e)
-            traceback.print_exc()
-        finally:
-            if local_count >= batch:
-                pbar.update(local_count)
-                local_count = 0
-            queue.task_done()
+    gc.disable()
 
-async def migrate_collection(
-    collection,
-    migrate_fn,
-    tgfs,
-    workers=20,
-    queue_size=500,
-    total_count = None,
-    name=None,
-    last_id=None
-):
-    queue = asyncio.Queue(maxsize=queue_size)
+    txn = env.begin(write=True)
+    count = 0
 
-    with tqdm(total=total_count, desc=name) as pbar:
-        worker_tasks = [
-            asyncio.create_task(
-                worker(f"W{i}", queue, migrate_fn, tgfs, pbar)
-            )
-            for i in range(workers)
-        ]
+    with open(Path(INPUT_BSON, "file.bson"), "rb") as f:
+        for old in decode_file_iter(f):
 
-        async for doc in batch_fetch(collection, batch_size=queue_size, last_id=last_id):
-            await queue.put(doc)
+            media_id, update = build_files_update(old)
+            key = str(media_id).encode()
 
-        for _ in worker_tasks:
-            await queue.put(None)
+            existing = txn.get(key)
+            if existing:
+                doc = decode(existing)
+            else:
+                doc = {"_id": media_id}
 
-        await queue.join()
-        await asyncio.gather(*worker_tasks)
+            deep_set(doc, update)
+            txn.put(key, encode(doc))
 
-async def batch_fetch(collection, batch_size=500, last_id=None):
-    try:
-        while True:
-            query = {}
-            if last_id:
-                query["_id"] = {"$gt": last_id}
+            count += 1
+            if count % WRITE_BATCH == 0:
+                txn.commit()
+                txn = env.begin(write=True)
+                print(f"[files] processed {count}")
 
-            docs = await collection.find(query)\
-                .sort("_id", 1)\
-                .limit(batch_size)\
-                .to_list(length=batch_size)
+    txn.commit()
+    gc.enable()
+    env.sync()
+    env.close()
 
-            if not docs:
-                break
 
-            for doc in docs:
-                yield doc
+def write_files_bson():
+    env = lmdb.open(LMDB_PATH, readonly=True, lock=False)
 
-            last_id = docs[-1]["_id"]
-    finally:
-        print("Finished fetching from collection.")
-        print("last_id:", last_id)
+    with env.begin() as txn, open(FILES_BSON, "wb") as out:
+        cursor = txn.cursor()
+        for _, value in cursor:
+            out.write(value)
 
-async def main():
-    bot_id=getenv("BOT_ID") or input("Enter Your Main Bot ID: ")
-    if not bot_id.isdigit():
-        print("Invalid Bot ID")
-        return
-    global BOT_ID
-    BOT_ID = int(bot_id)
-    old_uri = getenv("OLD_URI") or input(f"Enter Old Mongo URI: ")
-    old_dbname = getenv("OLD_DBNAME") or input("Enter Old Database Name(Default: F2LxBot): ") or "F2LxBot"
-    new_uri = getenv("NEW_URI") or input(f"Enter New Mongo URI: ")
-    new_dbname = getenv("NEW_DBNAME") or input("Enter New Database Name(Default: TGFS): ") or "TGFS"
-    if old_dbname == new_dbname and old_uri == new_uri:
-        print("Old and New MongoDB configurations cannot be the same.")
-        return
-    tgfs = MongoTGFS()
-    await tgfs.connect(new_uri, new_dbname)
-    await tgfs.init_db()
-    mongo_client = AsyncIOMotorClient(old_uri)
-    old_db = mongo_client[old_dbname]
+    env.close()
 
-    workers = getenv("WORKERS") or input("Enter number of worker tasks for migration (Default: 20): ")
-    workers = int(workers) if workers.isdigit() else 20
-    queue_size = getenv("QUEUE_SIZE") or input("Enter no of documents to pre fetch from DB (Default: 500): ")
-    queue_size = int(queue_size) if queue_size.isdigit() else 500
 
-    print("Migrating files...")
-    await migrate_collection(
-        old_db.file,
-        migrate_old_file,
-        tgfs,
-        workers=workers,
-        queue_size=queue_size,
-        total_count=await old_db.file.count_documents({}),
-        name="Files",
-        last_id=None
-    )
+def write_maplinks_bson():
+    with open(Path(INPUT_BSON, "file.bson"), "rb") as f, open(MAPLINKS_BSON, "wb") as out:
+        for old in decode_file_iter(f):
+            decoded = FileId.decode(old["file_id"])
+            out.write(encode({
+                "_id": old["_id"],
+                "media_id": decoded.media_id,
+                "user_id": int(old["user_id"]),
+            }))
 
-    print("Migrating users...")
-    await migrate_collection(
-        old_db.users,
-        migrate_old_users,
-        tgfs,
-        workers=workers,
-        queue_size=queue_size,
-        total_count=await old_db.users.count_documents({}),
-        name="Users",
-        last_id=None
-    )
+def write_users_bson():
+    user_ids = {}
+    with open(Path(INPUT_BSON, "blacklist.bson"), "rb") as f:
+        for doc in decode_file_iter(f):
+            user_ids[doc["id"]] = doc["ban_date"]
 
-    print("Migrating blacklist...")
-    await migrate_collection(
-        old_db.blacklist,
-        migrate_blacklist,
-        tgfs,
-        workers=workers,
-        queue_size=queue_size,
-        total_count=await old_db.blacklist.count_documents({}),
-        name="Blacklist",
-        last_id=None
-    )
+    with open(Path(INPUT_BSON, "users.bson"), "rb") as f, open(USERS_BSON, "wb") as coll:
+        for doc in decode_file_iter(f):
+            encoded_doc = encode({
+                "_id": doc["id"],
+                "ban_date": user_ids.get(doc["id"], None),
+                "curt_op": 0,
+                "join_date": normalize_time(doc["join_date"]),
+                "op_id": 0,
+                "preferred_lang": "en",
+                "warns": 0
+            })
+            coll.write(encoded_doc)
 
-    print("Setting version...")
-    await tgfs.config.update_one(
-        {"_id": "VERSION"},
-        {"$set": {"value": "0.0.1"}},
-        upsert=True,
-    )
-    mongo_client.close()
-asyncio.run(main())
+if __name__ == "__main__":
+    print("▶reducing files into LMDB")
+    migrate_files_lmdb()
+
+    print("▶ writing files.bson")
+    write_files_bson()
+
+    print("▶ writing maplinks.bson")
+    write_maplinks_bson()
+
+    print("▶ writing users.bson")
+    write_users_bson()
